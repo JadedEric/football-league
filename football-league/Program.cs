9edd1c7d4424f99f8846a4a406b38060542aca7c
Eric Truter <eric.truter@live.com>
2023-02-28 22:19:37 +0200

    feature: initial check-in
    - initial check in of the project
    - basic workflow in place

---
+++ football-league/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace football_league
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
